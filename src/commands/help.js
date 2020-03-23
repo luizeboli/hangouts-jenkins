89036b1df4ee0833f3174c08f86ba58ad675e5e0
Luiz Felicio <unifelicio@gmail.com>
2020-03-23 17:16:22 -0300

    fix: help command alias

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'ping',
-  description: 'Ping!',
+  name: 'help',
+  description: 'Used to list all available commands',
   execute(req, res) {
     return res.send({ text: "Here's all available commands you can use: \n Teste de quebra de linha" });
   },
