35e0fd028b542d3ed34456ce0b5d6af74f88b408
Luiz Felicio <unifelicio@gmail.com>
2020-03-23 16:58:37 -0300

    feat: add eslint and commands treat

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const commands = require('./commands');
+
 const PORT = process.env.PORT || 3000;
-const bodyParser = require('body-parser')
+const app = express();
+const botCommands = new Map();
+
+Object.keys(commands).map((key) => {
+  botCommands.set(commands[key].name, commands[key]);
+});
 
 app.use(bodyParser.json());
 
-app.post("/jenkins", (req, res) => {
-  console.log(req.body)
+app.post('/jenkins', (req, res) => {
+  console.log(req.body);
   const { space, type, message } = req.body || {};
-  
-  if (type === "ADDED_TO_SPACE" && space.type === "ROOM") {
+
+  const args = message.text.split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (type === 'ADDED_TO_SPACE' && space.type === 'ROOM') {
     res.send({ text: `Thanks for adding me to ${space.displayName}` });
   }
 
-  if (type === "MESSAGE") {
-    res.send({ text: `Your message was: ${message.text}`})
+  if (type === 'MESSAGE') {
+    // res.send({ text: `Your message was: ${message.text}`})
+
+    if (!botCommands.has(command)) res.send({ text: 'Sorry, I didnt recognize this command' });
   }
 });
 
