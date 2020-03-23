f762ddd5849b6879ebb5fc3148dbdfd69e5f6bda
Luiz Felicio <unifelicio@gmail.com>
2020-03-23 15:37:43 -0300

    feat: heroku test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+
+app.post("/top-secret-bot", (req, res) => {
+  const { space, type, message } = req.body || {};
+  
+  if (type === "ADDED_TO_SPACE" && space.type === "ROOM") {
+  res.send({ text: `Thanks for adding me to ${space.displayName}` });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
