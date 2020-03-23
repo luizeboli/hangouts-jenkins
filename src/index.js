618836f6beabf408b0090055063edef214637d95
Luiz Felicio <unifelicio@gmail.com>
2020-03-23 15:39:54 -0300

    feat: heroku test

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ app.use(bodyParser.json());
 
 app.post("/top-secret-bot", (req, res) => {
   const { space, type, message } = req.body || {};
+
+  console.log(req.body)
   
   if (type === "ADDED_TO_SPACE" && space.type === "ROOM") {
   res.send({ text: `Thanks for adding me to ${space.displayName}` });
   }
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
 });
 
 app.listen(PORT, () => {
