fc9ebd412c3441289204c6de3c74f1b891a69e72
Luiz Felicio <unifelicio@gmail.com>
2020-03-23 17:26:35 -0300

    refactor: help command style

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'help',
   description: 'Used to list all available commands',
   execute(req, res) {
-    return res.send({ text: "Here's all available commands you can use: \n*help*: list all commands\n*buildST*: start building process of static-ticket, *must* specify one branch." });
+    return res.send({ text: "Here's all available commands you can use: \n\n*help*: list all commands\n*buildST*: start building process of static-ticket on QA, *must* specify one branch." });
   },
 };
