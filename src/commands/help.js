38712ddc746757b73edcbd8143f00bc9478e63dd
Luiz Felicio <unifelicio@gmail.com>
2020-03-23 17:24:33 -0300

    feat: add help commands

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'help',
   description: 'Used to list all available commands',
   execute(req, res) {
-    return res.send({ text: "Here's all available commands you can use: \n Teste de quebra de linha" });
+    return res.send({ text: "Here's all available commands you can use: \n*help*: list all commands\n*buildST*: start building process of static-ticket, *must* specify one branch." });
   },
 };
