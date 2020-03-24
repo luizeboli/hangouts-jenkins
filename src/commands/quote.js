809b5d3b2c29ce7ca9bc9ec7c5495962ab2e2b17
Luiz Felicio <unifelicio@gmail.com>
2020-03-24 17:47:25 -0300

    fix: change random quote api

--- src/commands/quote.js
+++ src/commands/quote.js
@@ -5,6 +5,24 @@ module.exports = {
   description: 'receive a single random quote',
   execute(req, res) {
     axios.get('https://api.quotable.io/random')
-      .then(({ data }) => res.send({ text: `_"${data.content}"_\n - ${data.author}.` }));
+      .then(({ data }) => {
+        res.send({
+          cards: [{
+            header: {
+              title: '1001 Jenkins',
+              subtitle: 'Your quote',
+              imageUrl: 'https://lh3.googleusercontent.com/proxy/fI_xSzgrxu6Ckw7OSHbutybNupGcnX2tyqnoVG3qZSmDDQcPDJ2YvVwuj5ZM3wi7hzb-CArqASXsre6Mdiv9GsndmYi7CuWeAQdko5VhjreSlxBVqW5qur6F9Q=s88-c-mo',
+              imageStyle: 'IMAGE',
+            },
+            sections: [{
+              widgets: [{
+                textParagraph: {
+                  text: `<i>${data.content}</i><br /><br />- ${data.author}`,
+                },
+              }],
+            }],
+          }],
+        });
+      });
   },
 };
