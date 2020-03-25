6c81cf9b4c04048c72249564927fbcd4964dbbdb
Luiz Felicio <unifelicio@gmail.com>
2020-03-25 10:28:15 -0300

    refactor: success message

--- src/commands/build.js
+++ src/commands/build.js
@@ -49,7 +49,28 @@ module.exports = {
           return postMessage(options);
         }
 
-        options.data.text = `<${senderName}>, Build ${stdout.substring(stdout.length - 17)}${link}`;
+        options.data.cards = [{
+          header: {
+            title: '1001 Jenkins',
+            subtitle: 'Build succeeded 😁',
+            imageUrl: 'https://lh3.googleusercontent.com/proxy/fI_xSzgrxu6Ckw7OSHbutybNupGcnX2tyqnoVG3qZSmDDQcPDJ2YvVwuj5ZM3wi7hzb-CArqASXsre6Mdiv9GsndmYi7CuWeAQdko5VhjreSlxBVqW5qur6F9Q=s88-c-mo',
+            imageStyle: 'IMAGE',
+          },
+          sections: [{
+            widgets: [{
+              textParagraph: { text: `Finished ${stdout.substring(stdout.length - 17)}` },
+              buttons: [{
+                textButton: {
+                  text: 'OPEN JENKINS CONSOLE',
+                  onClick: {
+                    openLink: { url: link },
+                  },
+                },
+              }],
+            }],
+          }],
+        }];
+
         return postMessage(options);
       });
     } catch (error) {
